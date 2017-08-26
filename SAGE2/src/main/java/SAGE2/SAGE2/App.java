b05bab1819c0db9821fc4034e6d8261f5212a89e
Nicolas Reboul <nicolas.reboul31@gmail.com>
2017-08-26 16:10:20 +0200

    First commit : kafka dependency, maaven project

---
+++ SAGE2/src/main/java/SAGE2/SAGE2/App.java
@@ -0,0 +1,13 @@
+package SAGE2.SAGE2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
